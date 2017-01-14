c5feffab3e4db7febbb0610da84ff6040fc08383
Vlad <vlad@ghn.me>
2017-01-14 17:02:40 -0600

    Fix Slash Command

--- handler.js
+++ handler.js
@@ -6,21 +6,43 @@ const slack = require('serverless-slack');
 // The function that AWS Lambda will call
 exports.slackListener = slack.handler.bind(slack);
 
-// Interactive Message handler
-slack.on('/vbot hi', (msg, bot) => {
-  bot.reply({
-    "text": "Hello " + msg.user_name + "! How would you like to greet the channel?",
+// Slash Command handler (https://api.slack.com/slash-commands)
+slack.on('/vbot', (msg, bot) => {
+  let userName = msg.user_name
+  let messageText = msg.text
+
+  let usage = {
     "attachments": [{
-      "fallback": "actions",
-      "callback_id": "greetings_click",
-      "actions": [
-        { "type": "button", "name": "Wave", "text": ":wave:", "value": ":wave:" },
-        { "type": "button", "name": "Hello", "text": "Hello", "value": "Hello" },
-        { "type": "button", "name": "Howdy", "text": "Howdy", "value": "Howdy" },
-        { "type": "button", "name": "Hiya", "text": "Hiya", "value": "Hiya" }
-      ]
+      "title": "USAGE:",
+      "title_link": "https://github.com/vghn/vbot/blob/master/README.md",
+      "color": "#36a64f",
+      "fields": [{
+        "title": "/vbot hi",
+        "value": "Choose how to say hi to the channel",
+        "short": false
+      }]
     }]
-  });
+  };
+
+  switch (messageText) {
+    case 'hi':
+      bot.replyPrivate({
+        "text": `Hello ${userName}! How would you like to greet the channel?`,
+        "attachments": [{
+          "fallback": "actions",
+          "callback_id": "greetings_click",
+          "actions": [
+            { "type": "button", "name": "Wave", "text": ":wave:", "value": ":wave:" },
+            { "type": "button", "name": "Hello", "text": "Hello", "value": "Hello" },
+            { "type": "button", "name": "Howdy", "text": "Howdy", "value": "Howdy" },
+            { "type": "button", "name": "Hiya", "text": "Hiya", "value": "Hiya" }
+          ]
+        }]
+      });
+      break;
+    default:
+      bot.replyPrivate(usage);
+  }
 });
 
 // Interactive Message handler
@@ -34,28 +56,5 @@ slack.on('greetings_click', (msg, bot) => {
 slack.on('reaction_added', (msg, bot) => {
   bot.reply({
     "text": ":wave:"
-  });
-});
-
-// Testing
-slack.on('/vbot test', (msg, bot) => {
-  bot.reply({
-    "text": "Just playing!"
-  });
-});
-
-// Slash Command handler (https://api.slack.com/slash-commands)
-slack.on('/vbot', (msg, bot) => {
-  bot.replyPrivate({
-    "attachments": [{
-      "title": "USAGE:",
-      "title_link": "https://github.com/vghn/vbot/blob/master/README.md",
-      "color": "#36a64f",
-      "fields": [{
-        "title": "/vbot hi",
-        "value": "Choose how to say hi to the channel",
-        "short": false
-      }]
-    }]
   });
 });
