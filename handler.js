8043f4afea430bef4f320207f910e00e7185928c
Vlad <vlad@ghn.me>
2017-01-14 12:28:40 -0600

    Improve Slack messages

--- handler.js
+++ handler.js
@@ -8,16 +8,28 @@ exports.slackListener = slack.handler.bind(slack);
 
 // Slash Command handler (https://api.slack.com/slash-commands)
 slack.on('/vbot', (msg, bot) => {
+  let usage = {
+    "attachments": [{
+      "title": "USAGE:",
+      "title_link": "https://github.com/vghn/vbot/blob/master/README.md",
+      "color": "#36a64f",
+      "fields": [{
+        "title": "/vbot hi",
+        "value": "Choose how to say hi to the channel",
+        "short": false
+      }]
+    }]
+  }
+
   let message = {
-    text: "Hello " + msg.user_name + "! How would you like to greet the channel?",
-    attachments: [{
-      fallback: 'actions',
-      callback_id: "greetings_click",
-      actions: [
-        { type: "button", name: "Wave", text: ":wave:", value: ":wave:" },
-        { type: "button", name: "Hello", text: "Hello", value: "Hello" },
-        { type: "button", name: "Howdy", text: "Howdy", value: "Howdy" },
-        { type: "button", name: "Hiya", text: "Hiya", value: "Hiya" }
+    "text": "Hello " + msg.user_name + "! How would you like to greet the channel?",
+    "attachments": [{
+      "callback_id": "greetings_click",
+      "actions": [
+        { "type": "button", "name": "Wave", "text": ":wave:", "value": ":wave:" },
+        { "type": "button", "name": "Hello", "text": "Hello", "value": "Hello" },
+        { "type": "button", "name": "Howdy", "text": "Howdy", "value": "Howdy" },
+        { "type": "button", "name": "Hiya", "text": "Hiya", "value": "Hiya" }
       ]
     }]
   };
@@ -27,25 +39,16 @@ slack.on('/vbot', (msg, bot) => {
       bot.replyPrivate(message);
       break;
     default:
-      bot.replyPrivate({
-        text: 'Unknown command!',
-        attachments: [{
-          title: "Hi",
-          text: "Choose how to say hi to the channel"
-        }]
-      });
+      bot.replyPrivate(usage);
   }
 });
 
 // Interactive Message handler
 slack.on('greetings_click', (msg, bot) => {
-  let message = {
-    // selected button value
-    text: msg.actions[0].value
-  };
-
   // public reply
-  bot.reply(message);
+  bot.reply({
+    text: msg.actions[0].value
+  });
 });
 
 // Reaction Added event handler
