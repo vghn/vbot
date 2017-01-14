ef874ac992e7bbd4a8907fed61ed822e2e22c56e
Vlad <vlad@ghn.me>
2017-01-14 14:22:58 -0600

    Test s3 download

--- handler.js
+++ handler.js
@@ -2,6 +2,27 @@
 
 // Include the serverless-slack bot framework
 const slack = require('serverless-slack');
+// Include AWS SDK
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+// Get SSH Key
+getSSHKey(){
+  // const s3Bucket = this.serverless.variables.service.custom.s3Bucket;
+  // const sshKey = this.serverless.variables.service.custom.sshKey;
+  const s3Bucket = 'vladgh';
+  const sshKey = 'secure/production/keys/deploy';
+
+  var params = {
+    Bucket: s3Bucket,
+    Key: sshKey
+  };
+  s3.getObject(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+  });
+  return data;
+}
 
 // The function that AWS Lambda will call
 exports.slackListener = slack.handler.bind(slack);
@@ -38,6 +59,9 @@ slack.on('/vbot', (msg, bot) => {
   switch (msg.text) {
     case 'hi':
       bot.replyPrivate(message);
+      break;
+    case 'ssh':
+      bot.replyPrivate(getSSHKey);
       break;
     default:
       bot.replyPrivate(usage);
