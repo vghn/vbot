bcd2f4480d40041d76ab471f468f3d19c2c333d4
Vlad <vlad@ghn.me>
2017-06-06 17:15:23 -0500

    Rename handler and slack function

--- slack.js
+++ slack.js
@@ -4,7 +4,7 @@
 const slack = require('serverless-slack');
 
 // The function that AWS Lambda will call
-exports.slackListener = slack.handler.bind(slack);
+exports.listener = slack.handler.bind(slack);
 
 // Slash Command handler (https://api.slack.com/slash-commands)
 slack.on('/vbot', (msg, bot) => {
